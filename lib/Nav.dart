c5c756d89a7d2154d92e147a2ea3d654f2e716fe
c12games <coolstone1337@ya.ru>
2020-12-24 23:15:20 +0300

    dvfdvvc vcbcv

---
+++ lib/Nav.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Nav extends StatefulWidget {
+  Nav({Key key}) : super(key: key);
+
+  @override
+  _NavState createState() => _NavState();
+}
+
+class _NavState extends State<Nav> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Services',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Me',
+          )
+        ],
+      ),
+    );
+  }
+}
