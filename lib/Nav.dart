ca2922d83ea02d4eeacbb948fe0c5f285f6d8565
c12games <coolstone1337@ya.ru>
2020-12-25 14:13:24 +0300

    Changing name of app

--- lib/Nav.dart
+++ lib/Nav.dart
@@ -7,25 +7,30 @@ class Nav extends StatefulWidget {
   _NavState createState() => _NavState();
 }
 
+int _curentIndex = 0;
+
 class _NavState extends State<Nav> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _curentIndex,
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.explore_outlined),
-            label: 'Services',
-          ),
-          BottomNavigationBarItem(
             icon: Icon(Icons.account_circle_outlined),
             label: 'Me',
-          )
+          ),
         ],
+        selectedItemColor: Theme.of(context).accentColor,
+        onTap: (int index) {
+          setState(() {
+            _curentIndex = index;
+          });
+        },
       ),
     );
   }
