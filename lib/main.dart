14eae479130da6d406f7fae22597fb5d99e29f6b
c12games <coolstone1337@ya.ru>
2020-12-26 22:09:58 +0300

    added a title

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "MCS",
       theme: ThemeData(
         primaryColor: Colors.white,
         accentColor: Colors.red,
