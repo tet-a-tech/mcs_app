df8e8b7397b24d448fe2ec8637a51d510b305fd5
c12games <coolstone1337@ya.ru>
2020-12-28 21:54:42 +0300

    nice cock

--- lib/Auth/AuthPages.dart
+++ lib/Auth/AuthPages.dart
@@ -14,12 +14,39 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).accentColor,
-        body: Center(
-          child: Container(
-            
-          )
-          ),
+      backgroundColor: Theme.of(context).accentColor,
+      body: Container(
+          child: Center(
+        child: Column(
+          children: [
+            Positioned(
+                top: 10,
+                left: 25,
+                child: Image(
+                    image: AssetImage('assets/images/mcs logo white.png'))),
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(
+                icon: Icon(Icons.mail_outline),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            TextFormField(
+              controller: passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                icon: Icon(Icons.lock_outline),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  context.read<AuthService>().signIn();
+                },
+                child: Text('Login'))
+          ],
+        ),
+      )),
     );
   }
 }
